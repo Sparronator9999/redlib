229f22def1520d6137fb1dcdca80e08ec347945c
Sparronator9999 <86388887+Sparronator9999@users.noreply.github.com>
2026-08-07 09:08:00 +1000

    format: indent HTML/CSS/JS with tabs

--- static/copy.js
+++ static/copy.js
@@ -1,9 +1,9 @@
 async function copy() {
-    await navigator.clipboard.writeText(document.getElementById('bincode_str').value);
+	await navigator.clipboard.writeText(document.getElementById('bincode_str').value);
 }
 
 async function set_listener() {
-    document.getElementById('copy').addEventListener('click', copy);
+	document.getElementById('copy').addEventListener('click', copy);
 }
 
 window.addEventListener('load', set_listener);
