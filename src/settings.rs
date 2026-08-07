8711bf9fa7294a4be04d32b036f73be873d28abf
Sparronator9999 <86388887+Sparronator9999@users.noreply.github.com>
2026-08-07 11:15:29 +1000

    Fix subscriptions and filters being deleted when updating preferences See redlib-org#562

--- src/settings.rs
+++ src/settings.rs
@@ -193,7 +193,7 @@ fn set_cookies_method(req: Request<Body>, remove_cookies: bool) -> Response<Body
 			// Increment subscriptions cookie number
 			subscriptions_number_to_delete_from += 1;
 		}
-	} else {
+	} else if remove_cookies {
 		// Remove unnumbered subscriptions cookie
 		response.remove_cookie("subscriptions".to_string());
 
@@ -244,7 +244,7 @@ fn set_cookies_method(req: Request<Body>, remove_cookies: bool) -> Response<Body
 			// Increment filters cookie number
 			filters_number_to_delete_from += 1;
 		}
-	} else {
+	} else if remove_cookies {
 		// Remove unnumbered filters cookie
 		response.remove_cookie("filters".to_string());
 
